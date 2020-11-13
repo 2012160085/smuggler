1cf9b750929fee65d577b3ceedf1b09b8d760c5e
2012160085 <lkbest0085@gmail.com>
2020-11-13 11:11:55 +0900

    a

--- example.py
+++ example.py
@@ -1,10 +1,12 @@
 from smuggler import smuggler
+from fence import fence
 import os
 
 # smuggler( file_name, num_of_bits_to_hide_in_a_byte, buffer_size_in_byte )
 
-s = smuggler("PyInstaller-3.6.tar.gz",6,8)
+#s = smuggler("PyInstaller-3.6.tar.gz",6,8)
 #s = smuggler("text.txt",1,8);
-s.setImageDir("Images")
-s.writeToImages()
+#s.setImageDir("Images")
+#s.writeToImages()
 
+f = fence("Images/PyInstaller-3.6.tar.gz.smg")
